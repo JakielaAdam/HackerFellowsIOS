2b5962fd644ba4e0dd1a2bf222e5a6d77a657aa1
JakielaAdam <jakielaa@mail.gvsu.edu>
2015-07-28 14:34:09 -0400

    Added initial iOS application.

---
+++ Hacker Fellows/Hacker Fellows/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hacker Fellows
+//
+//  Created by Hacker Fellows on 7/28/15.
+//  Copyright (c) 2015 Grand Circus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
