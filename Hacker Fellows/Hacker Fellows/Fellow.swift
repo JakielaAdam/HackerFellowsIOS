2898732102613b9e04148c19ac6bb9709e0386bf
JakielaAdam <jakielaa@mail.gvsu.edu>
2015-08-03 12:51:07 -0400

    Added the fellows model.

---
+++ Hacker Fellows/Hacker Fellows/Fellow.swift
@@ -0,0 +1,21 @@
+//
+//  Fellow.swift
+//  Hacker Fellows
+//
+//  Created by Hacker Fellows on 8/3/15.
+//  Copyright (c) 2015 Grand Circus. All rights reserved.
+//
+
+import Foundation
+
+class Fellow {
+    
+    var name: String
+    var bio: String
+    
+    init(name: String, bio: String) {
+        self.name = name
+        self.bio = bio
+    }
+    
+}
